6b605b2489284edd578c3c37657a83e63c8ecd8b
Edward Magafurov <edward801@yandex.ru>
2023-12-21 00:01:01 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте массив на 10 целых чисел. 
+//Напишите программу, которая определяет количество чётных чисел в массиве.
+
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+                Console.Write ( array[i] + " ");     
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if((array[i] % 2) ==0)
+        {
+            Console.Write(array[i] +" ");
+        }
+    }
