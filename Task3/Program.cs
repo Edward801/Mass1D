1de52d6dcd518659474aa68a8e89a6009ba6eae5
Edward Magafurov <edward801@yandex.ru>
2023-12-23 12:04:22 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿ //Задайте массив из вещественных чисел с ненулевой дробной частью. 
+ //Найдите разницу между максимальным и минимальным элементов массива.
+
+
+double[] array = new double[10];
+Random rand = new Random(10);
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(1, 100) + rand.NextDouble();
+        if (array[i] != 0)
+                Console.WriteLine( array[i] + " ");     
+    }
+  
+    // Инициализируем переменные для хранения максимального и минимального элементов
+        double max = array[0];
+        double min = array[0];
+
+    // Находим максимальный и минимальный элементы массива
+        for (int i = 0; i < array.Length; i++)
+        {
+            
+            if (array[i] > max)
+            {
+            max = array[i];
+            }
+
+            if (array[i] < min)
+            {
+            min = array[i];
+            }
+        }
+
+    // Выводим Максимальное и Минимальное число
+    Console.WriteLine();
+
+    Console.WriteLine("Минимальное число =" + min);
+    Console.WriteLine("Максимальное число =" + max);
+          
+    // Находим разницу между максимальным и минимальным числом.
+    double difference = max - min;
+
+    // Выводим результат
+    Console.WriteLine("Разница между максимальным и минимальным числами =" + difference);
